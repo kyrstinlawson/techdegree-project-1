0b33b19ff5eb8a2137cc9efc410784cd94ec141b
kyrstinlawson <kyrstin.lawson@gmail.com>
2022-03-30 14:08:48 -0700

    added the quotes array

--- js/script.js
+++ js/script.js
@@ -3,13 +3,48 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+//quotes array
+const quotes = [
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    source: "Oscar Wilde",
+    citation: "",
+    year: 1890,
+    tags: ["be-yourself", "honesty", "inspirational"]
+  },
+  {
+    quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    source: "Albert Einstein",
+    citation: "",
+    year: 1940,
+    tags: ["human-nature", "humor", "philosophy"]
+  },
+  {
+    quote: "So many books, so little time.",
+    source: "Frank Zappa",
+    citation: "",
+    year: "unknown",
+    tags: ["books", "humor"]
+  },
+  {
+    quote: "You only live once, but if you do it right, once is enough.",
+    source: "Mae West",
+    citation: "",
+    year: "unknown",
+    tags: ["humor", "life", "inspirational"]
+  },
+  {
+    quote: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+    source: "J.K. Rowling",
+    citation: "Harry Potter and the Goblet of Fire",
+    year: 2000,
+    tags: ["books", "honesty", "human-nature"]
+  }
+];
 
 
 
